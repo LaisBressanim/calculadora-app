0d78af84aa58c5f87cf7c8bf3766e4288937f24d
LaisBressanim <laismartim2009@gmail.com>
2026-03-03 12:27:57 -0300

    Criando o display

--- calculadora/App.js
+++ calculadora/App.js
@@ -4,8 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <StatusBar style="light" />
+
+     <View style={styles.displayContainer}>
+      <Text style={styles.displayText} numberOfLines={1}>
+        1000
+      </Text>    
+    </View>
+    <View style={styles.row}>
+
+    </View>
     </View>
   );
 }
@@ -13,8 +21,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f3a2d8ff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  displayContainer: {
+  flex: 2.5.
+  justifyContent: 'flex-end',
+  alignItems: 'flex-end',
+  paddingHorizontal: 20,
+  paddingBottom:20,
+  },
+
+  displayText: {
+    color: #000000,
+    fontSize: 72,
+    fontWeight: '300',
+  },
+  row: {
+     flex:1,
+     flexDirection: 'row',
+     justifyContent: 'space-between',
+     paddingHorizontal:10
+
+  }
+ 
 });
